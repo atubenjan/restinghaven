dde5dd316141e194db448be01f2ab9d77e6667ae
Atugonza Benjamin <153981563+atubenjan@users.noreply.github.com>
2024-10-31 22:55:11 +0300

    Update grave_mapping.php

--- grave_mapping.php
+++ grave_mapping.php
@@ -160,12 +160,14 @@
 <!-- /.content-wrapper -->
 
 <!-- Add Grave Mapping Modal -->
-<div class="modal fade" id="addGraveMappingModal">
+<div class="modal fade" id="modal-default">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="addGraveMappingModalLabel">Add New Grave Mapping</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
       </div>
       <div class="modal-body">
         <form method="POST">
